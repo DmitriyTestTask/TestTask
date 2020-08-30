cd2489ceb3fdb182300aea6c7e72f44051ea4b05
Dmitriy S <sakovetsdmytryi@gmail.com>
2020-08-30 15:06:45 +0300

    Update API tests

--- src/test/java/test_API/Test_API.java
+++ src/test/java/test_API/Test_API.java
@@ -5,15 +5,19 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 public class Test_API {
+    //registration form
+    private String nameParam = "test-1";
+    private String emailParam = "test-1@gmail.com";
+    private String passwordParam = "qwetest82";
 
-    private String nameParam = "1";
-    private String emailParam = "234@gmail.com";
-    private String passwordParam = "458iop";
-    private String idContact = "";
+    private String idContact = "5f4b92c45a9df1001716cece";
     private String addName = "1";
     private String addEmail = "qwe567@gmail.com";
     private String addPhone = "589123753";
     private String addType = "personal";
+    private String auth_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmNGI4MmZhNWE5ZGYxMDAxNzE2Y2VjYyIs" +
+            "ImlhdCI6MTU5ODc4NjA1NSwiZXhwIjoxNTk4ODIyMDU1fQ.AZAdZbw5a4vk51AC2AhhRDFeZrdjXRA1JBiSqPbDshs";
+
 
 
     @Test
@@ -25,43 +29,75 @@ public class Test_API {
     }
 
     @Test
-    public void loginUser() {
+    public void registrationNewUser(){
         RestAssured.given()
-                .param("email:", emailParam)
-                .param("password:", passwordParam)
-                .post("https://cryptic-retreat-46933.herokuapp.com/api/auth/login")
-                .then();
-//                .statusCode(200);
-//
+                .contentType(ContentType.JSON)
+                .body("{\n"+ "\"name\"  : \"test-1\" ,\n"  +
+                               "\"email\" : \"test-1@gmail.com\", \n" +
+                                "\"password\" : \"qwetest82\"}")
+                .post("https://cryptic-retreat-46933.herokuapp.com/api/users")
+                .then()
+                .statusCode(200);
     }
 
     @Test
-    public void registrationNewUser(){
+    public void loginUser() {
         RestAssured.given()
-                .param("name", nameParam)
-                .param("email", emailParam)
-                .param("password", passwordParam)
-                .post("https://cryptic-retreat-46933.herokuapp.com/api/users")
+                .contentType(ContentType.JSON)
+                .body("{\n"+ "\"email\" : \"test-1@gmail.com\", \n"+
+                             "\"password\" : \"qwetest82\"}" )
+                .post("https://cryptic-retreat-46933.herokuapp.com/api/auth/login")
                 .then()
                 .statusCode(200);
 
     }
+
+//    @Test
+//    public void loginUser() {
+//        RestAssured.given()
+//                .param("email", emailParam)
+//                .param("password", passwordParam)
+//                .post("https://cryptic-retreat-46933.herokuapp.com/api/auth/login")
+//                .then()
+//                .statusCode(200);
+//    }
+
+//    @Test
+//    public void addContact(){
+//        RestAssured.given()
+//                .header("x-auth-token", auth_token)
+//                .header("Content-Type", "Application/JSON")
+//
+//                .param("name", addName)
+//                .param("email", addEmail)
+//                .param("phone",addPhone)
+//                .param("type", addType)
+//                .post("https://cryptic-retreat-46933.herokuapp.com/api/contacts/")
+//                .then()
+//                .statusCode(200);
+//    }
+
     @Test
     public void addContact(){
         RestAssured.given()
-                .param("name", addName)
-                .param("email", addEmail)
-                .param("phone",addPhone)
-                .param("type", addType)
+                .header("x-auth-token", auth_token)
+                .header("Content-Type", "Application/JSON")
+//                .contentType(ContentType.JSON)
+                .body("{\n" + "\"name\":\"1\"        ,\n" +
+                                    "\"email\":\"2\", \n"+
+                                    "\"phone\":\"3\" , \n" +
+                                     "\" type\": \" professional\"+\n}")
                 .post("https://cryptic-retreat-46933.herokuapp.com/api/contacts/")
                 .then()
                 .statusCode(200);
     }
-
     @Test
     public void deleteContact(){
         RestAssured.given()
-                .param("id", idContact)
+                .header("x-auth-token", auth_token)
+                .header("Content-Type", "Application/JSON")
+//                .contentType(ContentType.JSON)
+//                .body("{\n" + "\"id\": \"5f4b7da55a9df1001716cecb\",+\n}" )
                 .delete("https://cryptic-retreat-46933.herokuapp.com/api/contacts/" + idContact)
                 .then()
                 .statusCode(200);
